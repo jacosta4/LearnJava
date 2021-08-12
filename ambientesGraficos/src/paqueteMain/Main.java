b45c62d06221ca4af4faae27a13735ce0a12a1fd
jacosta4 <jacosta441@gmail.com>
2021-08-11 21:01:53 -0500

    Clase de Interfaz grafica V1

---
+++ ambientesGraficos/src/paqueteMain/Main.java
@@ -0,0 +1,13 @@
+package paqueteMain;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MiPrimerFrame frame = new MiPrimerFrame();
+        frame.setVisible(true);
+    }
+}
+
+
